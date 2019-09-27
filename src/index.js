882caf1a5fad02d5ee167bee1a1c81f8d7ce3e43
KocMoHaWT <doncov.vladislav@gmail.com>
2019-09-27 15:30:36 +0300

    rename css files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/reset.css';
-import './css/style.css';
+import './scss/reset.scss';
+import './scss/style.scss';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
